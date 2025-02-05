d210ce41cdd54625407b242b5d7cba21176007d9
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-02-05 16:34:33 +1000

    Added tailwind to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-
 import './App.css'
 
-function App() {
+const App: React.FC = () => {
   const [count, setCount] = useState(0)
 
   
   return (
-    <>
+    <div className='App'>
+      <span className="text-blue-500">Taskify</span>
       <h1>Hello world</h1>
-    </>
+    </div>
   )
 }
 
