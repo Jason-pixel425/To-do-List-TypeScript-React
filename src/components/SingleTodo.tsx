ddbfc37924304251b1cf034b5873662586966846
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-02-11 15:22:45 +1000

    Ui complete

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -65,20 +65,21 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
 
 
     return (
-        <form className="todos_single flex w-full lg:w-[40%] rounded-sm p-5 mt-4 bg-[url(https://img.freepik.com/free-photo/crumpled-yellow-paper-background-close-up_60487-2390.jpg?size=626&ext=jpg)]" onSubmit={(e) => handleEdit(e, todo.id)}>
+        <form className="todos_single flex w-full lg:w-[95%] duration-200 hover:shadow-xl hover:scale-105 rounded-sm p-3 mt-4 bg-[url(https://img.freepik.com/free-photo/crumpled-yellow-paper-background-close-up_60487-2390.jpg?size=626&ext=jpg)]" onSubmit={(e) => handleEdit(e, todo.id)}>
+            
             {
                 edit ? (
                     <input ref={inputRef} className="bg-white grow text-xl p-1 border-none focus:outline-none" value={editTodo} onChange={(e => setEditTodo(e.target.value))}/>
                 ): (todo.isDone === true ? (
-                        <s className="todos_single-text grow p-1 text-left border-none text-[1.25rem] focus:outline-none">{todo.todo}</s>
+                        <s className={`todos_single-text  font-Merienda min-w-0 break-words grow flex-wrap p-1 text-wrap whitespace-normal  text-left border-none text-[1.25rem] focus:outline-none`}>{todo.todo}</s>
                         ) : (
-                            <span className="todos_single-text grow p-1 text-left border-none text-[1.25rem] focus:outline-none">
+                            <span className={`todos_single-text  font-Merienda min-w-0 break-words grow flex-wrap p-1 text-wrap whitespace-normal  text-left border-none text-[1.25rem] focus:outline-none`}>
                                 {todo.todo}
                             </span>
                         )
 
                 )}
-  
+           
             <div className="flex p-1 items-center">
                 {edit ? (
                         <>
@@ -87,8 +88,8 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
                         </>
                     ):(
                         <>
-                        <span className="icon ml-2 text-2xl cursor-pointer" onClick={() => handleEditState()}><AiFillEdit /></span>
-                        <span className="icon ml-2 text-2xl cursor-pointer" onClick={() => handleDelete(todo.id)}><AiFillDelete /></span>
+                        <span className={`icon ml-2 text-2xl ${todo.isDone? "cursor-not-allowed" : "cursor-pointer"}`} onClick={() => handleEditState()}><AiFillEdit /></span>
+                        <span className={`icon ml-2 text-2xl cursor-pointer`} onClick={() => handleDelete(todo.id)}><AiFillDelete /></span>
                         <span className="icon ml-2 text-2xl cursor-pointer" onClick={() => handleDone(todo.id)}><MdDone /></span>
                         </>
                         
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -9,17 +9,29 @@ interface Props {
 
 const ToDoList: React.FC<Props> = ({ todos, setTodos }) => {
 
-    const todoArr = todos.map(todo => {
-         return <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
-    })
+    // const todoArr = todos.map(todo => {
+    //      return <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
+    // })
+    
 
     return (
-        <div className="todos flex justify-evenly w-[90%] flex-wrap">
-            <ul className="flex justify-evenly w-[90%] flex-wrap"> 
-                {todoArr}
-            </ul>
+        <div className="container flex flex-col md:flex-row text-wrap mx-auto items-center w-[95%] mt-2 justify-beteween md:items-start">
+            <div className="todos w-[80%] mb-4 rounded-sm md:w-1/2 text-wrap p-5 flex flex-col items-center bg-cyan-500">
+            <h2 className="text-2xl font-Merienda">Active Tasks</h2>
+            { todos.map(todo => {
+                return todo.isDone ? null : <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
+            })
+            }
+            </div>
+            <div className="todos w-[80%] mb-4 rounded-sm md:w-1/2 text-wrap p-5 flex flex-col items-center bg-rose-300 items-center">
+            <h2 className="text-2xl font-Merienda">Completed Tasks</h2>
+            { todos.map((todo) => {
+                return todo.isDone ? <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} /> : null
+            })
+            }
+            </div>
         </div>
     )
 }
-
+//return todo.isDone ? <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} /> : null
 export default ToDoList
